e4ceeeb6fb40f9ee203c4d03c079075366ca9555
Francois <YellowIce_13009@Outlook.com>
2021-04-30 15:23:05 +0200

    modification et création de plusieurs pages

---
+++ src/components/pages/ProductPage.js
@@ -0,0 +1,43 @@
+import "./ProductPage.scss";
+import logo from '../images/vet_03.jpg'
+import Navbar from "../Navbar/Navbar";
+const ProductPage =() => {
+    return (
+        <>
+        <Navbar/>
+        <div className="productpage">
+            <div className="productpage-left">
+                <div className="left-image">
+                    <img src={logo} alt="product name"/>
+                </div>
+
+                <div className="left-info">
+                    <p className="left-name"> Product 1</p>
+                    <p> Price : 10.99 € </p>
+                    <p> tres bon produit </p>
+                </div>
+                <div className="productpage-left">
+                    <div className="left-info">
+                        <p> Price : <span> 10.99 € </span>
+                        </p>
+                        <p> Status : <span> In Stock</span></p>
+                        <p> Qty
+                            <select>
+                                <option value="1"> 1 </option>
+                                <option value="2"> 2 </option>
+                                <option value="3"> 3 </option>
+                                <option value="4"> 4 </option>
+                            </select>
+                        </p>
+                        <p>
+                            <button type="button"> Add to cart </button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default ProductPage
