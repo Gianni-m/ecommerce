44bed17d42c632fe2e728fdb9c848ef33b316277
Francois <YellowIce_13009@Outlook.com>
2021-05-16 15:22:02 +0200

    modification de la page gestion du stock

--- src/components/productManagement/stockDashboard/stockManagement/StockManagement.js
+++ src/components/productManagement/stockDashboard/stockManagement/StockManagement.js
@@ -1,45 +1,49 @@
 import "./StockManagement.scss"
 
+const products = [
+    {
+        id: 1212,
+        name: 'tshirt',
+        quantity: 3,
+        price: 32323.12,
+        size: 'L',
+    },
+    {
+        id: 23,
+        name: 'pull',
+        quantity: 59354,
+        price: 32323,
+        size: 'M',
+
+    },
+    {id: 535434,
+        name: 'Pantalon Sport',
+        quantity: 2323,
+        price: 32323.4343,
+        size: 'XXL',
+    },
+    {
+        id: 3232,
+        name: 'Casquette Vigile',
+        quantity: 1220,
+        price: 32.5,
+        size: 'S',
+    },
+]
 
 
 
 const StockManagement = () => {
 
 
-    const products = [
-        {
-            id: 1212,
-            name: 'tshirt',
-            quantity: 3,
-            price: 32323.12,
-        },
-        {
-            id: 23,
-            name: 'pull',
-            quantity: 59354,
-            price: 32323,
-
-        },
-        {id: 535434,
-            name: 'Pantalon Sport',
-            quantity: 2323,
-
-            price: 32323.4343,
-        },
-        {
-            id: 3232,
-            name: 'Casquette Vigile',
-            quantity: 1220,
-            price: 32.5,
-        },
-    ]
 
 const renderStock = (product, index) => {
     return (
         <tr key={index}>
             <td>{product.name} </td>
+            <td> {product.size} </td>
             <td> {product.quantity}</td>
-            <td> {product.price}</td>
+
         </tr>
 
 
@@ -48,23 +52,26 @@ const renderStock = (product, index) => {
 }
     return (
 
+        <div className="stockManagement">
+            <h1 className="title"> Gestion du stock</h1>
+
             <div className="table">
 
-
-                    <tr>
+                    <tr className="header-table">
                         <th>Name</th>
+                        <th>Size</th>
                         <th>Quantity</th>
-                        <th>Price</th>
+
                     </tr>
 
-                    <tbody className="ouis">
+                    <tbody className="body-table">
 
                     {products.map(renderStock)}
 
                     </tbody>
 
             </div>
-
+        </div>
     );
 }
 
